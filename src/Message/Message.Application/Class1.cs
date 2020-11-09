57cd3a2a959b8ae4f1fe8de565a2683bc7c9cb3b
OguzErdi <erdi.erkmen@gmail.com>
2020-11-10 01:12:10 +0300

    Add project files.

---
+++ src/Message/Message.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Message.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Message/Message.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Message.Core
+{
+    public class Class1
+    {
+    }
+}
